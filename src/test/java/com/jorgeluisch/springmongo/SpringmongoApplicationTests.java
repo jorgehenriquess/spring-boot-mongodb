fc6cdcb399ac7cfbdbe440584b55b4708d2c929e
Jorge Henriques <jorge.cavalcanti2@gmail.com>
2021-06-28 23:50:18 -0300

    Projeto criado

---
+++ src/test/java/com/jorgeluisch/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeluisch.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
