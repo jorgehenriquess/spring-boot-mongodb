e6857e9620b3f032c909d5f50d9753fdaafe8723
Jorge Henriques <jorge.cavalcanti2@gmail.com>
2021-06-29 00:55:37 -0300

    Conexao com base mongo

---
+++ src/main/java/com/jorgeluisch/springmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.jorgeluisch.springmongo.resources;
+
+import com.jorgeluisch.springmongo.domain.User;
+import com.jorgeluisch.springmongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
